8acbb1cae36606ac9d2ee824bcd2b7041d927d73
Rodrigo Santiago <rodrigo@santive.com>
2019-02-20 22:14:22 -0300

    Step 5 of "Tour of Heroes" angular tutorial completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[];  
 
   getHeroes(): void {
     this.heroService.getHeroes()
