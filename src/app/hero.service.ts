e53103cee43f458f0d4d2ff877749524eb85e89a
Rodrigo Santiago <rodrigo@santive.com>
2019-02-22 18:21:25 -0300

    Fix of handleError operation parameter

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -55,7 +55,7 @@ export class HeroService {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=$id`))
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
 }
